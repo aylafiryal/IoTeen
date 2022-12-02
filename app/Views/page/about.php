92308e956b891543cf9f593be9923177d5857439
Aulia Firyal Syarifa <auliafiryalsyarifa@gmail.com>
2022-12-02 22:09:55 +0700

    Delete app/Views/page directory

--- app/Views/page/about.php
+++
@@ -1,7 +0,0 @@
-<?= $this->extend('layout/template'); ?>
-<?= $this->section('content'); ?>
-
-<h1>About</h1>
-<p>Noooooosaka Yuuuuuuuuma</p>
-
-<?= $this->endSection(); ?>
--- app/Views/page/home.php
+++
@@ -1,6 +0,0 @@
-<?= $this->extend('layout/template'); ?>
-<?= $this->section('content'); ?>
-
-<h1>Home</h1>
-
-<?= $this->endSection(); ?>
